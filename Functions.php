37f8a234e8cdff5bde1c71961f104a0a2b8828ac
cccpast <coltpex13fix@gmail.com>
2018-03-11 17:01:08 +0900

    add and rename functions

--- Functions.php
+++ Functions.php
@@ -8,21 +8,45 @@ class Functions
      * 2つの日付の差分を取得
      * @param string $date1
      * @param string $date2
+     * @return array $diffDate
      */
-    public static function dateDiff($date1, $date2)
+    public static function diffDate($date1, $date2)
     {
-        // date(Array) idx:0 Y-m-d, idx:1 H:i:s
+        $diffDate = array();
+        // date(Array) idx:0 Y-m-d, idx:1 H:i
         $date1 = explode(" ", $date1);
         $date2 = explode(" ", $date2);
         // Y-m-d
         $ymd1 = strtotime($date1[0]);
         $ymd2 = strtotime($date2[0]);
         $ymd_diff = ($ymd2 - $ymd1) / (60 * 60 * 24);
-        var_dump($ymd_diff);
-        // H:i:s
+        $diffDate["ymd"] = $ymd_diff;  // int
+        // H:i
         $his1 = strtotime($date1[1]);
         $his2 = strtotime($date2[1]);
-        $diff_his = date("H:i:s", ($his2 - $his1));
-        var_dump($diff_his);
+        $diff_his = date("H:i", ($his2 - $his1));
+        $diff_h = date("H", ($his2 - $his1));
+        $diff_i = date("i", ($his2 - $his1));
+        $diffDate["hi"] = $diff_his;  // string
+        $diffDate["h"] = $diff_h;  // string
+        $diffDate["i"] = $diff_i;  // string
+        return $diffDate;
+    }
+
+    /**
+     * 日数を時間に変換し、加算する
+     * @param array $date
+     * @return string $time
+     */
+    public static function convertTime($date)
+    {
+        // 1日以上経過している場合
+        if ($date["ymd"] != 0) {
+            $hour = 24 * $date["ymd"];  // 1日を時間に変換
+            $hour = ($date["h"] - 0) + $hour;
+            $time = (string)$hour . ":" . $date["i"];
+            return $time;
+        }
+        return $date["hi"];
     }
 }
--- Test.php
+++ Test.php
@@ -1,6 +1,9 @@
 <?php
 require_once __dir__ . "/Functions.php";
 
-$date1 = "2018-01-01 01:00:00";
-$date2 = "2018-02-03 23:00:00";
-Functions::dateDiff($date1, $date2);
+$date1 = "2018-01-01 00:15:00";
+$date2 = "2018-01-02 01:00:25";
+$date = Functions::diffDate($date1, $date2);
+$hour = Functions::convertTime($date);
+var_dump($date);
+var_dump($hour);
